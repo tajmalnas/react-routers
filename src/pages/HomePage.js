ea81e4d23f14d1f9c8db370f14ef900a6af1b11b
tajmalnas <tajjuddin.malnas21@pccoepune.org>
2023-04-14 22:52:50 +0530

    used useNavigate

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+
+    const navigate = useNavigate();
+
+    function navigateHandler(){
+        navigate('/products');
+    }
+
   return (
-    <main>
+    <>
         <h1>HomePage</h1>
         <p>Go to <Link to='/products'>go to products page</Link></p>
-    </main>
+        <button onClick={navigateHandler}>Navigate</button>
+    </>
   )
 }
 
